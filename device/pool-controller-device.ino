6ad1fdd526ceecf515b50e9ba3aebd668053febc
Brent Lemons <brent_lemons@yahoo.com>
2023-02-26 10:45:55 -0600

    start the cloud api

--- device/pool-controller-device.ino
+++ device/pool-controller-device.ino
@@ -1,10 +1,18 @@
 #include <WiFiClientSecure.h>
 #include <MQTTClient.h>
 #include <ArduinoJson.h>
-#include "WiFi.h"
+#include <WiFi.h>
 #include <PubSubClient.h>
 
 #include <SPIFFS.h>
+
+// Date and time functions using a DS3231 RTC connected via I2C and Wire lib
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 #define MQTT_KEEP_ALIVE (900)
 #define MQTT_TIMEOUT    (900)
@@ -79,6 +87,22 @@ void setup() {
     Serial.println("Failed to mount file system");
     return;
   }
+
+  // if (! rtc.begin()) {
+  //   Serial.println("Couldn't find RTC");
+  //   Serial.flush();
+  //   while (1) delay(10);
+  // }
+
+  // if (rtc.lostPower()) {
+  //   Serial.println("RTC lost power, let's set the time!");
+  //   // When time needs to be set on a new device, or after a power loss, the
+  //   // following line sets the RTC to the date & time this sketch was compiled
+  //   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //   // This line sets the RTC with an explicit date & time, for example to set
+  //   // January 21, 2014 at 3am you would call:
+  //   // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  // }
 
   // Initialize the relay switches
   initInp(INP1);
